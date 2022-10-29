b0d7a35f1a8e58ea3ad623600d4bc8b8fe32be83
aito <BoxPistols@users.noreply.github.com>
2022-10-29 23:49:15 +0900

    set icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 // 画像をそれぞれimportします。パスに注意してください。
 import CoverImage from "../images/bg.jpg"
 import ProfileImage from "../images/profile.png"
+import { FaTwitter, FaGithub } from "react-icons/fa"
 
 function Header(props) {
   return (
@@ -26,8 +27,18 @@ function Header(props) {
               <h1 className="title-text">あなたの名前</h1>
               <h3 className="title-text">あなたの肩書(ex.Engineer)</h3>
               <ul className="social-icons">
-                <li className="icon-link">{/* TODO */}</li>
-                <li className="icon-link">{/* TODO */}</li>
+                <li className="icon-link">
+                  {/* リンク先(href)は任意のURLを設定してください */}
+                  <a href="https://twitter.com/">
+                    <FaTwitter color="white" size="2rem" />
+                  </a>
+                </li>
+                <li className="icon-link">
+                  {/* リンク先(href)は任意のURLを設定してください */}
+                  <a href="https://github.com/">
+                    <FaGithub color="white" size="2rem" />
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
