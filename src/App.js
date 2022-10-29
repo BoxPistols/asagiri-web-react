09672e56f2c1629bdd54c9f70458bdddfb3bf258
aito <BoxPistols@users.noreply.github.com>
2022-10-29 23:20:10 +0900

    formatter csscomb prettier / Base Layout

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import logo from "./logo.svg"
 import "./App.css"
 import Header from "./components/Header"
 import Contact from "./components/Contact"
+
 import Skill from "./components/Skills"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Header name="ヘッダー" />
-          <Skill name="スキル" />
-          <Contact name="お問い合わせ" />
-        </p>
-      </header>
+      <Header name="ヘッダー" />
+      <Skill name="スキル" />
+      <Contact name="お問い合わせ" />
     </div>
   )
 }
