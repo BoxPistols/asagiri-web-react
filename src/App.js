82cff71a2ee3a1212b29ae5942ffe02d74c2fad0
aito <BoxPistols@users.noreply.github.com>
2022-10-30 20:28:09 +0900

    axios basic

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import "./App.css"
 
 function App() {
   return (
-    <React.StrictMode>
+    <>
       <div className="App">
         <Header name="ヘッダー" />
         <Skill name="スキル" />
         <Contact name="お問い合わせ" />
       </div>
-    </React.StrictMode>
+    </>
   )
 }
 
